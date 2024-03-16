7d4ffa8ca4407e67c5971e7b939e7b52c987d0f5
ShounakMahata18 <shounakmahata18@gmail.com>
2024-03-16 12:52:35 +0530

    adding to remote repo

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meterial Requirement Prcessing</title>
+    <link rel="stylesheet" href="CSS/home.css">
+</head>
+<body>
+    <header>
+        <h1>Meterial Requirement Prcessing</h1>
+        <div id="right-nav">
+            <ul>
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#contact-us">CONTACT US</a></li>
+                <li><a href="login.php">LOGIN</a></li>
+            </ul>
+        </div>
+    </header>
+    <div id="home">
+        <div id="left-home">
+            <h2>Meterial Requirement Prcessing</h2>
+            <p>Material requirements planning (MRP) is a method used to manage and plan for the materials needed in manufacturing processes. It involves calculating the quantities of materials required for production based on the production schedule and desired inventory levels. MRP helps ensure that materials are available when needed, minimizing shortages and excess inventory. It also helps in managing the procurement process by determining when and how much to order.</p>
+        </div>
+        <div id="right-home">   
+            <img src="image/processing1.jpeg" alt="error">
+        </div>
+    </div>
+    <div id="about">
+        <div id="left-about">
+            <img src="image/processing2.jpeg" alt="error">
+        </div>
+        <div id="right-about">   
+            <h2>Furniture Manufacture</h2>
+            <p>Here We are taking table and chair as the order product, according to the ordered product user can show the raw material needed to delhiver their product.</p>
+        </div>
+    </div>
+    <div id="contact-us">
+        <h4>Contact Us</h4>
+        <div id="dev">
+
+            <div id="dev1">
+                <h5>Shounak Mahata</h5>
+                <p>shounakmahata18@gmail.com</p>
+            </div>
+            <div id="dev2">
+                <h5>Senjuti Saha</h5>
+                <p>sahasenjuti796@gmail.com</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
